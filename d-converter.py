6045bc7c4ed86b8b8b782d059f337d2312fe458c
Diego Gaglione <72506891+DiegoDev-It@users.noreply.github.com>
2021-04-22 17:32:37 +0200

    Working on code
    Added optional path for temporary files, auto delete the copy of file to be converted and commented code.

--- d-converter.py
+++ d-converter.py
@@ -1,17 +1,19 @@
 import ffmpeg
+import os
 import tkinter as tk
 from tkinter import filedialog
 
-print("File converter CLI by diwgo3sk")
-print("Input file name including encoder (example: file.mp4)")
-format = input()
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
-stream = ffmpeg.input(file_path)
+#os.chdir("/Files")# Optional, for Windows 10 add complete path to Files folder or the program will be crash if uncomment.
+
+root = tk.Tk() #Set alias for TKinter.
+root.withdraw() #Set window method.
+
+file_path = filedialog.askopenfilename() #Asks for path of file to be converted.
+new_file_path = filedialog.asksaveasfilename() #Asks for name, encoder and path of converted file.
+stream = ffmpeg.input(file_path) #Add file to be converted to ffmpeg input.
 try:
-    stream = ffmpeg.output(stream, format)
-    print (format)
+    stream = ffmpeg.output(stream, new_file_path) #Save converted file.
 except:
-    print("FFmpeg doesn't contains this encoder")
-ffmpeg.run(stream)
+    print("FFmpeg doesn't contains this encoder") #Exceptions.
+ffmpeg.run(stream) #Run ffmpeg
+os.remove(file_path) #Deletes the copy of file to be converted in Files folder.
